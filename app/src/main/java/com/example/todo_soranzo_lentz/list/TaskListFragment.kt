34baac31fe0fb63253b09b777bf2d26fbb40fc48
florian.lentz <florian.lentz@ensiie.fr>
2023-01-15 23:35:15 +0100

    TP3 Ok

--- app/src/main/java/com/example/todo_soranzo_lentz/list/TaskListFragment.kt
+++ app/src/main/java/com/example/todo_soranzo_lentz/list/TaskListFragment.kt
@@ -6,25 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.todo_soranzo_lentz.R
+import com.example.todo_soranzo_lentz.data.Api
 import com.example.todo_soranzo_lentz.databinding.FragmentTaskListBinding
 import com.example.todo_soranzo_lentz.detail.DetailActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.util.*
+import kotlinx.coroutines.launch
 
 class TaskListFragment : Fragment() {
-    private var taskList = listOf(
+    /*private var taskList = listOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3")
-    )
+    )*/
     val adapterListener : TaskListListener = object : TaskListListener {
         override fun onClickDelete(task: Task) {
-            taskList = taskList - task;
-            refreshAdapter()
+            /*taskList = taskList - task;
+            refreshAdapter()*/
+            viewModel.remove(task)
+            viewModel.refresh()
         }
 
         override fun onClickEdit(task: Task) {
@@ -35,16 +39,23 @@ class TaskListFragment : Fragment() {
     }
     private val adapter = TaskListAdapter(adapterListener)
     private lateinit var binding : FragmentTaskListBinding
+
     val createTask = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         val task = result.data?.getSerializableExtra("task") as Task
-        taskList = (taskList + task!!)
-        refreshAdapter()
+        /*taskList = (taskList + task!!)
+        refreshAdapter()*/
+        viewModel.add(task)
+        viewModel.refresh()
     }
     val editTask = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         val task = result.data?.getSerializableExtra("task") as Task
-        taskList = taskList.map { if (it.id == task.id) task else it }
-        refreshAdapter()
+        /*taskList = taskList.map { if (it.id == task.id) task else it }
+        refreshAdapter()*/
+        viewModel.edit(task)
+        viewModel.refresh()
     }
+
+    private val viewModel: TasksListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,13 +64,13 @@ class TaskListFragment : Fragment() {
     ): View? {
         binding = FragmentTaskListBinding.inflate(layoutInflater)
         val rootView = binding.root
-        adapter.submitList(taskList)
+        //adapter.submitList(taskList)
         return rootView
     }
 
-    fun refreshAdapter() {
+    /*fun refreshAdapter() {
         adapter.submitList(taskList)
-    }
+    }*/
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.recyclerView.adapter = adapter
@@ -67,5 +78,27 @@ class TaskListFragment : Fragment() {
             val intent = Intent(context, DetailActivity::class.java)
             createTask.launch(intent)
         }
+        viewModel.refresh()
+        lifecycleScope.launch { // on lance une coroutine car `collect` est `suspend`
+            viewModel.tasksStateFlow.collect { newList ->
+                // cette lambda est executée à chaque fois que la liste est mise à jour dans le VM
+                /*taskList = newList
+                refreshAdapter()*/
+                adapter.submitList(newList)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            mySuspendMethod()
+        }
+        viewModel.refresh()
+    }
+
+    private suspend fun mySuspendMethod() {
+        val user = Api.userWebService.fetchUser().body()!!
+        view?.findViewById<TextView>(R.id.userTextView)?.text = user.name
     }
 }
